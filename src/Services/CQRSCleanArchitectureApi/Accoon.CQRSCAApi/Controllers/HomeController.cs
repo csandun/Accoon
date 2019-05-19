0dd84eaafea5b2b0a67245a6bbfadec429e566a5
Chathuranga Sandun <chathurangasandunkumara@gmail.com>
2019-05-19 17:33:40 +0530

    add api to solution

---
+++ src/Services/CQRSCleanArchitectureApi/Accoon.CQRSCAApi/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Accoon.CQRSCAApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [Route("")]
+        public IActionResult Get()
+        {
+            return Ok("Running api");
+        }
+    }
+}
