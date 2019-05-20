b97756268ae8a8bd83c20f1f9248171e4dce6601
Chathuranga Sandun <chathurangasandunkumara@gmail.com>
2019-05-20 23:51:50 +0530

    references

--- src/Services/CQRSCleanArchitectureApi/Application/Accoon.CQRSCAApi.Application/UserCases/CreateCustomer/CreateCustomerCommand.cs
+++ src/Services/CQRSCleanArchitectureApi/Application/Accoon.CQRSCAApi.Application/UserCases/CreateCustomer/CreateCustomerCommand.cs
@@ -12,3 +12,19 @@ namespace Accoon.CQRSCAApi.Application.UserCases.CreateCustomer
         public int Age { get; set; }
     }
 }
+
+//https://app.pluralsight.com/player?course=clean-architecture-patterns-practices-principles&author=matthew-renze&name=clean-architecture-patterns-practices-principles-m3&clip=3&mode=live
+
+//https://github.com/mmacneil/CleanAspNetCoreWebApi/blob/master/src/Web.Api.Core/UseCases/LoginUseCase.cs
+
+//https://alexisalulema.com/2018/04/30/ddd-clean-architecture-template/
+
+//https://github.com/alulema/DDD-CleanArchitectureTemplate/tree/master/CleanDds.Application.CommandStack/Rates
+
+//https://hackernoon.com/applying-clean-architecture-on-web-application-with-modular-pattern-7b11f1b89011
+
+//https://github.com/ivanpaulovich/clean-architecture-manga/blob/master/manga/src/Manga.Application/UseCases/Register/RegisterUseCase.cs
+
+//https://github.com/JasonGT/NorthwindTraders/blob/master/Northwind.Application/Customers/Commands/CreateCustomer/CreateCustomerCommand.cs
+
+//https://www.youtube.com/watch?v=Zygw4UAxCdg
