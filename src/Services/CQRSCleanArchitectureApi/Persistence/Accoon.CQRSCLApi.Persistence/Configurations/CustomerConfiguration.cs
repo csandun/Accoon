2bd2c26f086a15a9d6946404ad1e44abec17ee87
Chathuranga Sandun <chathurangasandunkumara@gmail.com>
2019-05-22 01:00:23 +0530

    persistence layer

---
+++ src/Services/CQRSCleanArchitectureApi/Persistence/Accoon.CQRSCLApi.Persistence/Configurations/CustomerConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Accoon.CQRSCLApi.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Accoon.CQRSCLApi.Persistence.Configurations
+{
+    public class CustomerConfiguration : IEntityTypeConfiguration<Customer>
+    {
+        public void Configure(EntityTypeBuilder<Customer> builder)
+        {
+            builder.HasKey(e => e.Id);
+            builder.Property(e => e.Name).HasMaxLength(100).IsRequired();
+            builder.Property(e => e.Age).IsRequired();
+        }
+    }
+}
