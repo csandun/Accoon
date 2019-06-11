69b0012eb4e2addbe072809c4c477c8e0e7b84b9
chathurangasandun <csk@tiqri.com>
2019-06-11 18:51:55 +0300

    rabbit mq send request

---
+++ src/Services/CQRSCleanArchitectureApi/Application/Accoon.CQRSCAApi.Application/UserCases/CreateCustomer/CustomerCreatedIntegrationEvent.cs
@@ -0,0 +1,18 @@
+﻿using Accoon.BuildingBlocks.EventBus.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Accoon.CQRSCAApi.Application.UserCases.CreateCustomer
+{
+    public class CustomerCreatedIntegrationEvent: IntegrationEvent
+    {
+        public Guid CustomerId { get; set; }
+
+        public CustomerCreatedIntegrationEvent(Guid customerId)
+        {
+            this.CustomerId = customerId;
+
+        }
+    }
+}
